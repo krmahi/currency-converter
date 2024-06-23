e5b35ee89986fe32b1f4b1a6bb20015f1ce20611
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-06-23 20:04:25 +0530

    :initial tailwind commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 function App() {
-  return;
+  return (
+    <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center">
+      <CurrencyConverter />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CurrencyConverter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CurrencyConverter(props) {
+  return <div></div>;
+}
+
+export default CurrencyConverter;
