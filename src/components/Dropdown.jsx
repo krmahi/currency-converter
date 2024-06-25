88b192c0fa5099b048e36b7e7ffdc6fb44a28bf0
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-06-26 01:26:33 +0530

    add: finished adding favorite functionality and localStorage connectivity && style: styled components for currency Dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,27 +1,62 @@
 import React from "react";
+import { HiOutlineStar, HiStar } from "react-icons/hi2";
 
 const currencyDropdown = ({
   currencies,
-  currenc,
+  currency,
   setCurrency,
-  favorite,
+  favorites,
   handleFavorite,
   title = "",
 }) => {
-  // console.log(currencies);
+  const isFavorite = (curr) => favorites.includes(curr);
   return (
     <div>
-      <label htmlFor={title}>{title}</label>
-      <div>
-        <select>
-          {currencies?.map((currency) => {
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {title}
+      </label>
+      <div className="mt-1 relative">
+        <select
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          {favorites.map((currency) => {
             return (
-              <option value={currency} key={currency}>
+              <option
+                className="bg-neutral-200"
+                value={currency}
+                key={currency}
+              >
                 {currency}
               </option>
             );
           })}
+          {currencies
+            .filter((c) => !favorites.includes(c))
+            .map((currency) => {
+              return (
+                <>
+                  <option value={currency} key={currency}>
+                    {currency}
+                  </option>
+                </>
+              );
+            })}
         </select>
+        <button
+          onClick={() => handleFavorite(currency)}
+          className="absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5"
+        >
+          {isFavorite(currency) ? (
+            <HiStar color="#DCBF00" />
+          ) : (
+            <HiOutlineStar />
+          )}
+        </button>
       </div>
     </div>
   );
